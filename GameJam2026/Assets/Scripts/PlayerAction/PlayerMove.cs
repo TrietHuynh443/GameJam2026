583c7205d3131270a0a6b412fb2bc8e45920beda
thanh ki <thanhki634@gmail.com>
2026-01-31 02:49:19 +0700

    feat: add sprint ability

--- GameJam2026/Assets/Scripts/PlayerAction/PlayerMove.cs
+++ GameJam2026/Assets/Scripts/PlayerAction/PlayerMove.cs
@@ -4,28 +4,40 @@ using UnityEngine.InputSystem;
 public class PlayerWalk : MonoBehaviour
 {
     public InputActionAsset InputActions;
+
     public float walkSpeed = 5f;
+    public float sprintMultiplier = 1.6f;
 
     private InputAction _move;
+    private InputAction _sprint;
 
     private void Awake()
     {
-        _move = InputActions.FindActionMap("Player").FindAction("Move");
+        var playerMap = InputActions.FindActionMap("Player");
+
+        _move = playerMap.FindAction("Move");
+        _sprint = playerMap.FindAction("Sprint");
     }
 
     private void OnEnable()
     {
         _move.Enable();
+        _sprint.Enable();
     }
 
     private void OnDisable()
     {
         _move.Disable();
+        _sprint.Disable();
     }
 
     private void Update()
     {
         Vector2 input = _move.ReadValue<Vector2>();
-        transform.position += (Vector3)(input * walkSpeed * Time.deltaTime);
+
+        bool isSprinting = _sprint.IsPressed();
+        float speed = isSprinting ? walkSpeed * sprintMultiplier : walkSpeed;
+
+        transform.position += (Vector3)(input * speed * Time.deltaTime);
     }
 }
